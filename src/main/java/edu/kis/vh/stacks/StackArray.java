d3e1c71cd6f0f01143e2ab2dbaf84d64669b3f6f
Jakub Olczak <kuba.olczak.jo@gmail.com>
2017-03-22 20:04:58 +0100

    3.1.5 Dodanie interfejsu IStack i aktualizacja nazw metod

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.stacks;
 
-public class StackArray{
+public class StackArray implements IStack{
 	
 	private final int EMPTY_STACK_INDICATOR = -1;//final dodany wcześniej
 	private final int FULL_STACK_INDICATOR = 11;//final dodany wcześniej
@@ -12,26 +12,31 @@ public class StackArray{
 	public int getTotal() {
 		return total;
 	}
-
+	
+	@Override
 	public void push(int i) {
 		if (!isFull())
 			ITEMS[++total] = i;
 	}
 	
+	@Override
 	public boolean isEmpty() {
 		return total == EMPTY_STACK_INDICATOR;
 	}
 	
+	@Override
 	public boolean isFull() {
 		return total == FULL_STACK_INDICATOR;
 	}
 	
+	@Override
 	public int top() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
 		return ITEMS[total];
 	}
 	
+	@Override
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
--- src/main/java/edu/kis/vh/stacks/list/StackList.java
+++ src/main/java/edu/kis/vh/stacks/list/StackList.java
@@ -1,13 +1,15 @@
 package edu.kis.vh.stacks.list;
 
-public class StackList {
+import edu.kis.vh.stacks.IStack;
+
+public class StackList implements IStack {
 	
 	//Przydatny w punkcie 3.1.4 jest refactor -> rename
 	
 	private Node lastNode; //po enkapsulacji modyfikator zmienił się na prywatny i dodano getter i setter
 	private static final int EMPTY_STACK_INDICATOR = -1; //final dodany wcześniej
 
-	public void pushElement(int nodeNumber) {
+	public void push(int nodeNumber) {
 		if (lastNode == null)
 			lastNode = new Node(nodeNumber);
 		else {
@@ -17,22 +19,22 @@ public class StackList {
 		}
 	}
 
-	public boolean empty() {
+	public boolean isEmpty() {
 		return lastNode == null;
 	}
 
-	public boolean full() {
+	public boolean isFull() {
 		return false;
 	}
 
-	public int peek() {
-		if (empty())
+	public int top() {
+		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
 		return lastNode.getValue();
 	}
 
 	public int pop() {
-		if (empty())
+		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
 		int topValue = lastNode.getValue();
 		lastNode = lastNode.getPreviousValue();
